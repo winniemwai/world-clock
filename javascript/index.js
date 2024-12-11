e31f469f2bd13851c853f683bcadc64bda0630b0
winniemwai <winniemwai5451@gmail.com>
2024-12-11 15:06:22 +0300

    added basic JavaScript to the document

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,27 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let nairobiElement = document.querySelector("#nairobi");
+  if (nairobiElement) {
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
+
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM	Do YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+updateTime();
+setInterval(updateTime, 1000);
